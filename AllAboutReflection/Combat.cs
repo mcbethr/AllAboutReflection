de6702971f72442181b73097564e9bd0b0e26080
rmcbeth-at-732376098976 <ryan.mcbeth@gmail.com>
2021-01-05 12:00:51 -0500

    Initial commit

---
+++ AllAboutReflection/Combat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AllAboutReflection
+{
+    public class Combat
+    {
+        ICharacter _CurrentCharacter;
+
+        public Combat (ICharacter CurrentCharacter)
+        {
+            _CurrentCharacter = CurrentCharacter;
+        }
+        
+        public void RearAttack()
+        {
+            switch(_CurrentCharacter.CharacterClass)
+            {
+                case EnumCharacterClass.Thief:
+                {
+                        Thief MyThief = new Thief();
+                        MyThief.Backstab();
+                }
+                break;
+                case EnumCharacterClass.ProjectManager:
+                    {
+                        ProjectManager MyProjectManager = new ProjectManager();
+                        MyProjectManager.Backstab();
+                    }
+                    break;
+                case EnumCharacterClass.Wizard:
+                    {
+                        _CurrentCharacter.Attack();
+                    }
+                break;
+                case EnumCharacterClass.Fighter:
+                    {
+                        _CurrentCharacter.Attack();
+                    }
+                    break;
+                case EnumCharacterClass.Cleric:
+                    {
+                        _CurrentCharacter.Attack();
+                    }
+                    break;
+            }
+
+
+        }
+        
+
+    }
+}
