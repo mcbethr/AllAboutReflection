8c8f34acd6cedcad9fb3f4d4a4a842917ffde8dc
rmcbeth-at-732376098976 <ryan.mcbeth@gmail.com>
2021-01-05 12:03:03 -0500

    Added Reflection Files

---
+++ BetterCombat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AllAboutReflection
+{
+    public class BetterCombat
+    {
+       
+        ICharacter _CurrentCharacter;
+
+        public BetterCombat(ICharacter CurrentCharacter)
+        {
+            _CurrentCharacter = CurrentCharacter;
+        }
+
+        public void RearAttack()
+        {
+            var CharacterType = _CurrentCharacter.GetType();
+
+            MethodInfo BackStabMethodInfo = CharacterType.GetMethod("Backstab");
+
+            if (BackStabMethodInfo == null)
+            {
+                _CurrentCharacter.Attack();
+            }
+            else
+            {
+                object BackStabCharacterInstance = Activator.CreateInstance(CharacterType, null);
+                BackStabMethodInfo.Invoke(BackStabCharacterInstance, null);
+            }
+
+
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AllAboutReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Fighter RegularRyan = new Fighter();
+            ProjectManager CircleBackJack = new ProjectManager();
+
+             //Better Way with reflection
+            BetterCombat MyBetterCombat = new BetterCombat(RegularRyan);
+            MyBetterCombat.RearAttack();
+
+            MyBetterCombat = new BetterCombat(CircleBackJack);
+            MyBetterCombat.RearAttack();
+
+            
+        }
+
+    }
+
+    public interface ICharacter
+    {
+        public EnumCharacterClass CharacterClass { get; }
+        public void Attack(){}
+    }
+
+    public abstract class Character : ICharacter
+    {
+        public abstract EnumCharacterClass CharacterClass { get; }
+
+        public void Attack()
+        {
+            Console.WriteLine("Character Attacks");
+        }
+
+        public void Defend()
+        {
+            Console.WriteLine("Character Defends");
+        }
+
+    }
+
+    class Thief : Character
+    {
+        public override EnumCharacterClass CharacterClass { get { return EnumCharacterClass.Thief; } }
+
+        public void Backstab()
+        {
+            Console.WriteLine("Thief Backstabs;");
+        }
+    }
+
+    class ProjectManager : Character
+    {
+        public override EnumCharacterClass CharacterClass { get { return EnumCharacterClass.Thief; } }
+
+        public void Backstab()
+        {
+            Console.WriteLine("Project Manager Backstabs;");
+        }
+    }
+
+    class Salesperson : Character
+    {
+        public override EnumCharacterClass CharacterClass { get { return EnumCharacterClass.Thief; } }
+
+        public void Backstab()
+        {
+            Console.WriteLine("Salesperson Backstabs;");
+        }
+    }
+
+    class Wizard : Character
+    {
+        public override EnumCharacterClass CharacterClass { get { return EnumCharacterClass.Wizard; } }
+    }
+
+    class Fighter : Character
+    {
+        public override EnumCharacterClass CharacterClass { get { return EnumCharacterClass.Fighter; } }
+    }
+
+    class Cleric : Character
+    {
+        public override EnumCharacterClass CharacterClass { get { return EnumCharacterClass.Cleric; } }
+    }
+
+    public enum EnumCharacterClass
+    {
+        Thief,
+        Fighter,
+        Cleric,
+        Wizard,
+        ProjectManager
+
+    }
+}
